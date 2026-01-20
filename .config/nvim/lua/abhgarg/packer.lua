bac6a565b98c47902aa2074ac6ece05b2fa508e4
Abhinav Garg <abhinavgarg2004@gmail.com>
2026-01-20 12:03:20 +0530

    Update polybar and picom for tranlucence Fix clipboard for nvim Update rofi config Add asus control settings

--- .config/nvim/lua/abhgarg/packer.lua
+++ .config/nvim/lua/abhgarg/packer.lua
@@ -82,5 +82,15 @@ return require('packer').startup(function(use)
     use('ggandor/leap.nvim')
     use('ThePrimeagen/harpoon')
     use('github/copilot.vim')
+    use({
+    'MeanderingProgrammer/render-markdown.nvim',
+    after = { 'nvim-treesitter' },
+    requires = { 'nvim-mini/mini.nvim', opt = true },            -- if you use the mini.nvim suite
+    -- requires = { 'nvim-mini/mini.icons', opt = true },        -- if you use standalone mini plugins
+    -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+    config = function()
+        require('render-markdown').setup({})
+    end,
+})
 end)
 
